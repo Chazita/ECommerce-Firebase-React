9ea0aa217cfb0cbd37d8b8fc2b8bc274579e44ca
unknown <chazarreta.matias.1138@gmail.com>
2020-11-22 20:38:21 -0300

    firerbase config

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,14 +4,14 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "",
-	authDomain: "",
-	databaseURL: "",
-	projectId: "",
-	storageBucket: "",
-	messagingSenderId: "",
-	appId: "",
-	measurementId: "",
+	apiKey: "AIzaSyAt-yxFObo0ljsvDxi0_Jh8zmgrvVCbfiA",
+	authDomain: "ecommerce-516f2.firebaseapp.com",
+	databaseURL: "https://ecommerce-516f2.firebaseio.com",
+	projectId: "ecommerce-516f2",
+	storageBucket: "ecommerce-516f2.appspot.com",
+	messagingSenderId: "901473080949",
+	appId: "1:901473080949:web:b8dfc2c3487a1acbde8a3c",
+	measurementId: "G-6QM2341LJ0",
 };
 
 const fb = firebase.initializeApp(firebaseConfig);
